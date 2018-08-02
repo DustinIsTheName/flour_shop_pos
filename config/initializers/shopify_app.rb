561bd2c657997328461effebf03cc4983ca2e90d
Dustin Milam <darklordsushi@yahoo.com>
2018-08-02 09:53:17 -0400

    added config.session_repository = Shop to initializer

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,6 +1,7 @@
 ShopifyApp.configure do |config|
   config.api_key = ENV['API_KEY']
   config.secret = ENV['SECRET']
+  config.session_repository = Shop
   # config.redirect_uri = "#{APP_URL}/auth/shopify/callback"
   config.scope = "read_orders, read_products"
   config.embedded_app = true
