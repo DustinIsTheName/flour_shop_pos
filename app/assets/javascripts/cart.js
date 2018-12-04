84e0b27a9810815ce9d1f64ed1bac2e0df929a6f
Dustin Milam <darklordsushi@yahoo.com>
2018-12-04 13:19:33 -0500

    revert back

--- app/assets/javascripts/cart.js
+++ app/assets/javascripts/cart.js
@@ -45,8 +45,18 @@ function addKioskOrder() {
     line_items.push($(this).data('line-item'));
   });
 
+  var customer = {
+    first_name: order.first_name,
+    last_name: order.last_name,
+    email: order.email,
+    note: "Customer from Kiosk"
+  };
+
   ShopifyPOS.fetchCart({
     success: function (cart) {
+      cart.setCustomer(customer, {success: function(cart) {
+        console.log("Customer added");
+      }, error: errorCallback})
 
       cart.addProperties(order.note_attributes, {
         success: function(cart) {
@@ -57,10 +67,7 @@ function addKioskOrder() {
         }
       })
 
-      var cart_note = '';
       line_items.forEach(function(line_item, index) {
-        cart_note += line_item.title + ' \n';
-
         cart.addLineItem({variant_id: line_item.variant_id, quantity: line_item.quantity}, {success: function(cart) {
           console.log("Item added to cart");
         }, error: errorCallback});
@@ -68,37 +75,23 @@ function addKioskOrder() {
         var propertiesObject = {};
         line_item.properties.forEach(function(property) {
           propertiesObject[property.name] = property.value;
-          cart_note += property.name + ': ' + property.value + '\n';
         });
 
-        // cart.addLineItemProperties(index, propertiesObject, {
-        //   success: function(cart) {
-        //     console.log("Successfully added properties to item")
-        //     // $('#kiosk-orders').append('<div class="section">'+JSON.stringify(cart)+'</div>');
-        //     // ShopifyPOS.flashNotice(JSON.stringify(cart))
-        //   },
-        //   error: function(errors) {
-        //     console.log("Failed to add properties to item")
-        //     ShopifyPOS.flashError("Failed to add properties to lineitem")
-        //   }
-        // });
+        cart.addLineItemProperties(index, propertiesObject, {
+          success: function(cart) {
+            console.log("Successfully added properties to item")
+            // $('#kiosk-orders').append('<div class="section">'+JSON.stringify(cart)+'</div>');
+            // ShopifyPOS.flashNotice(JSON.stringify(cart))
+          },
+          error: function(errors) {
+            console.log("Failed to add properties to item")
+            ShopifyPOS.flashError("Failed to add properties to lineitem")
+          }
+        });
       });
 
-      var customer = {
-        first_name: order.first_name,
-        last_name: order.last_name,
-        email: order.email,
-        note: cart_note
-      };
-
-      cart.setCustomer(customer, {success: function(cart) {
-        console.log("Customer added");
-        $('#kiosk-orders').append('<div class="section">'+cart+'</div>')
-      }, error: errorCallback})
-
       successCallback("Loaded Order");
-
-      // ShopifyPOS.Modal.close();
+      ShopifyPOS.Modal.close();
     },
     error: function (errors) {
       ShopifyPOS.flashError("Oops! Something went wrong.")
